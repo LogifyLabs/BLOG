c206ce5255b4c6d067dc1304bb45e157755fedd2
hyeyumpeople <hyeyum@humanartmuseum.com>
2026-04-23 10:48:10 +0900

    Quartz sync: Apr 23, 2026, 10:48 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,10 +13,12 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {/* {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year} */}
-        </p>
+        <hr />
+        <p>© 2026 **Logify-Lab**. Some Rights Reserved.</p>
+        <p>이 저작물은 [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/deed.ko)에 따라 이용할 수 있습니다.</p>
+        <p><a href="mailto:hyeyum@humanartmuseum.com">hyeyum@humanartmuseum.com</a></p>
+        
+        {/* 만약 나중에 다시 링크를 넣고 싶을 때를 위해 남겨두는 코드 */}
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
