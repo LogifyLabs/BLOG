92b118d0375a6f1f90ad80d4cc86e71549de7054
hyeyumpeople <hyeyum@humanartmuseum.com>
2026-04-23 11:17:17 +0900

    Quartz sync: Apr 23, 2026, 11:17 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -9,7 +9,7 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <hr />
+        {/* <hr /> 이 줄을 지웠습니다! 이제 선이 안 나올 거예요. */}
         <p>© 2026 <strong>Logify-Lab</strong>. All Rights Reserved.</p>
         <p>
           이 저작물은 <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.ko" target="_blank">CC BY-NC-ND 4.0</a>에 따라 이용할 수 있습니다.
