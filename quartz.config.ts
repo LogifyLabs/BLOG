99c1718a4021fd2fd769ad79f1ce8f1e79e5710a
hyeyumpeople <hyeyum@humanartmuseum.com>
2026-04-23 09:52:59 +0900

    Quartz sync: Apr 23, 2026, 9:52 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "LogifyLab", // 제목을 하나로 합쳤습니다.
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "logifylab.com",
+    baseUrl: "logifylab.com", // 여기에 쉼표가 잘 붙어있어야 합니다.
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -88,7 +88,6 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
     ],
   },
